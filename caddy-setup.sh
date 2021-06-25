60bc7d96acd0e6451d23832f471a56b8790e5d3e
daniel-stoyanov <84960050+daniel-stoyanov@users.noreply.github.com>
2021-06-25 07:57:15 +0300

    Update caddy-setup.sh

--- caddy-setup.sh
+++ caddy-setup.sh
@@ -10,13 +10,21 @@ mkdir caddy
 
 cd caddy
 
-wget https://raw.githubusercontent.com/ColoradoStark/caddy-ssl-multisite/main/docker-compose.yml
+wget https://raw.githubusercontent.com/daniel-stoyanov/caddy-wordpress-multi/main/docker-compose.yml
 
-wget https://github.com/ColoradoStark/caddy-ssl-multisite/raw/master/websites.tar
+wget https://github.com/daniel-stoyanov/caddy-wordpress-multi/raw/main/websites.tar
 
-wget https://raw.githubusercontent.com/ColoradoStark/caddy-ssl-multisite/master/db-variables.env
+wget https://raw.githubusercontent.com/daniel-stoyanov/caddy-wordpress-multi/main/db-variables-tm.env
 
-wget https://raw.githubusercontent.com/ColoradoStark/caddy-ssl-multisite/master/wp-variables.env
+wget https://raw.githubusercontent.com/daniel-stoyanov/caddy-wordpress-multi/main/db-variables-vg.env
+
+wget https://raw.githubusercontent.com/daniel-stoyanov/caddy-wordpress-multi/main/db-variables-vr.env
+
+wget https://raw.githubusercontent.com/daniel-stoyanov/caddy-wordpress-multi/main/wp-variables-tm.env
+
+wget https://raw.githubusercontent.com/daniel-stoyanov/caddy-wordpress-multi/main/wp-variables-vr.env
+
+wget https://raw.githubusercontent.com/daniel-stoyanov/caddy-wordpress-multi/main/wp-variables-vg.env
 
 tar -xvf websites.tar
 
